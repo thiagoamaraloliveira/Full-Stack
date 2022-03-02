213be41cf4de1f034c37a671ee5d67647f1dbc24
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-02 13:09:33 -0400

    export and import knex config

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
-const app = require("express")()
-const consign = require ('consign')
+const app = require("express")();
+const consign = require("consign");
+const db = require("./config/db");
+
+app.db = db;
 
 consign()
-    .then('./config/middlewares.js')
-    .then('./api')
-    .then('./config/routes.js')
-    .into(app)
+  .then("./config/middlewares.js")
+  .then("./api")
+  .then("./config/routes.js")
+  .into(app);
 
-app.listen(3000 , () =>{
-    console.log("Backend executando....")
-})
+app.listen(3000, () => {
+  console.log("Backend executando....");
+});
