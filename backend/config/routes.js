7f07f4dded3d408efbfab0c58f4a84c54bfa409b
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-03 12:16:51 -0400

    created methods for category : save,get,remove

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,5 +1,16 @@
 module.exports = (app) => {
   app.route("/users").post(app.api.user.save).get(app.api.user.get);
 
-  app.route("/users/:id").put(app.api.user.save).get(app.api.user.getById);;
+  app.route("/users/:id").put(app.api.user.save).get(app.api.user.getById);
+
+  app
+    .route("/categories")
+    .post(app.api.category.save)
+    .get(app.api.category.get);
+
+  app
+    .route("/categories/:id")
+    .put(app.api.category.save)
+    .get(app.api.category.getById)
+    .delete(app.api.category.remove);
 };
