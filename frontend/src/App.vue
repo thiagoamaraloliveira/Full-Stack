84951488615d6b4fd920f55e7e22889fc55f91dc
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-06 14:20:42 -0400

    created Vuex

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <Header title="ProwTech - Base de Conhecimento" :hideToggle="false"/>
+  <div id="app" :class="{ 'hide-menu': !isMenuVisible }">
+    <Header title="ProwTech - Base de Conhecimento" :hideToggle="false" />
     <Menu />
     <Content />
     <Footer />
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import Header from "@/components/template/Header";
 import Menu from "@/components/template/Menu";
 import Content from "@/components/template/Content";
@@ -16,6 +17,7 @@ import Footer from "@/components/template/Footer";
 export default {
   name: "App",
   components: { Header, Menu, Content, Footer },
+  computed: mapState(["isMenuVisible"]),
 };
 </script>
 
@@ -34,9 +36,16 @@ body {
   display: grid;
   grid-template-rows: 60px 1fr 40px;
   grid-template-columns: 300px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "menu footer";
+}
+
+#app.hide-menu{
   grid-template-areas: 
   "header header"
-  "menu content"
-  "menu footer";
+  "content content"
+  "footer footer";
 }
 </style>
