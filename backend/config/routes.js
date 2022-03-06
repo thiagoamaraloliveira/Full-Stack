ef5df770d676e3f9f5cf1685b1db6bc183a88d70
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-06 10:20:36 -0400

    created and added authenticate for all endpoints

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -3,32 +3,53 @@ module.exports = (app) => {
   app.post("/signin", app.api.auth.signin);
   app.post("/validateToken", app.api.auth.validateToken);
 
-  app.route("/users").post(app.api.user.save).get(app.api.user.get);
+  app
+    .route("/users")
+    .all(app.config.passport.authenticate())
+    .post(app.api.user.save)
+    .get(app.api.user.get);
 
-  app.route("/users/:id").put(app.api.user.save).get(app.api.user.getById);
+  app
+    .route("/users/:id")
+    .all(app.config.passport.authenticate())
+    .put(app.api.user.save)
+    .get(app.api.user.getById);
 
   app
     .route("/categories")
+    .all(app.config.passport.authenticate())
     .post(app.api.category.save)
     .get(app.api.category.get);
 
   // Cuidado com a ordem!
 
-  app.route("/categories/tree").get(app.api.category.getTree);
+  app
+    .route("/categories/tree")
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.getTree);
 
   app
     .route("/categories/:id")
+    .all(app.config.passport.authenticate())
     .put(app.api.category.save)
     .get(app.api.category.getById)
     .delete(app.api.category.remove);
 
-  app.route("/articles").get(app.api.article.get).post(app.api.article.save);
+  app
+    .route("/articles")
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.get)
+    .post(app.api.article.save);
 
   app
     .route("/articles/:id")
+    .all(app.config.passport.authenticate())
     .get(app.api.article.getById)
     .put(app.api.article.save)
     .delete(app.api.article.remove);
 
-  app.route("/categories/:id/articles").get(app.api.article.getByCategory);
+  app
+    .route("/categories/:id/articles")
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.getByCategory);
 };
