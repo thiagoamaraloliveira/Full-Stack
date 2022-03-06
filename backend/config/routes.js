1501afafe63e67ae175dd8699a5cdcd8adb8c62a
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-06 11:48:45 -0400

    created and added backpoint get stats

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -54,4 +54,9 @@ module.exports = (app) => {
     .route("/categories/:id/articles")
     .all(app.config.passport.authenticate())
     .get(app.api.article.getByCategory);
+
+  app
+    .route("/stats")
+    .all(app.config.passport.authenticate())
+    .get(app.api.stat.get);
 };
