77f9334f937b140d545123e5fdb4d0b443fc567a
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-06 09:07:31 -0400

    created queries get all articles id from category

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -25,4 +25,6 @@ module.exports = (app) => {
     .get(app.api.article.getById)
     .put(app.api.article.save)
     .delete(app.api.article.remove);
+
+  app.route("/categories/:id/articles").get(app.api.article.getByCategory);
 };
