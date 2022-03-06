6794137293795dd7b1af9882fa999a6822f4e813
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-06 15:11:16 -0400

    created router config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,16 @@
-import 'font-awesome/css/font-awesome.css'
-import Vue from 'vue'
+import "font-awesome/css/font-awesome.css";
+import Vue from "vue";
 
-import App from './App'
+import App from "./App";
 
-import './config/bootstrap'
-import store from "./config/store"
+import "./config/bootstrap";
+import store from "./config/store";
+import router from "./config/router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
