9857cd863f0ffdba6d84bc6381be3c374e081278
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-07 06:37:40 -0400

    created toaster msgs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,15 @@ import Vue from "vue";
 import App from "./App";
 
 import "./config/bootstrap";
+import "./config/msgs";
 import store from "./config/store";
 import router from "./config/router";
 
 Vue.config.productionTip = false;
 
 //TEMPORARIO !!
-require("axios").defaults.headers.common["Authorization"] =" bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlRoaWFnbyBPbGl2ZWlyYSIsImVtYWlsIjoidGhpYWdvQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NDY1OTQ4NTIsImV4cCI6MTY0Njg1NDA1Mn0.gG8Zi7kUXdyTDDS6cHHb_Zcd9dDrwxepwfGk6UxK8Lo"
+require("axios").defaults.headers.common["Authorization"] =
+  " bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlRoaWFnbyBPbGl2ZWlyYSIsImVtYWlsIjoidGhpYWdvQGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NDY1OTQ4NTIsImV4cCI6MTY0Njg1NDA1Mn0.gG8Zi7kUXdyTDDS6cHHb_Zcd9dDrwxepwfGk6UxK8Lo";
 
 new Vue({
   store,
