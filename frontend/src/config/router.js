0e0a3a6bb80a02c0ed7d7d51a12863cb9a6093ed
Thiago Oliveira <thiagoamaral.oliveira@gmail.com>
2022-03-08 07:15:33 -0400

    adde token expirated method

--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -5,7 +5,9 @@ import Home from "@/components/home/Home";
 import AdminPages from "@/components/admin/AdminPages";
 import ArticlesByCategory from "@/components/article/ArticlesByCategory";
 import ArticleById from "@/components/article/ArticleById";
-import Auth from "@/components/auth/Auth"
+import Auth from "@/components/auth/Auth";
+
+import { userKey } from "@/global";
 
 Vue.use(VueRouter);
 
@@ -19,6 +21,7 @@ const routes = [
     name: "adminPages",
     path: "/admin",
     component: AdminPages,
+    meta: { requiresAdmin: true },
   },
   {
     name: "articlesByCategory",
@@ -37,7 +40,20 @@ const routes = [
   },
 ];
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: "history",
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const json = localStorage.getItem(userKey);
+
+  if (to.matched.some((record) => record.meta.requiresAdmin)) {
+    const user = JSON.parse(json);
+    user && user.admin ? next() : next({ path: "/" });
+  } else {
+    next();
+  }
+});
+
+export default router;
